2aee95baf962f84a3a270d80b4427a8a7f12dd8c
Muhammad Jamil <mjamil1339@gmail.com>
2021-07-27 23:22:13 +0700

    First Init

---
+++ 2713130/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="icon" href="../img/logo.png" type="image/icon type">
+  <link rel="stylesheet" href="css/style.css">
+  <title>SARS-COV2 Screening Result Verification</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+  <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+
+  <link rel="stylesheet" href="../eladmin/cs-skin-elastic.css">
+  <link rel="stylesheet" href="../eladmin/style.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+</body>
+
+<!-- showcase & nav section -->
+
+<!-- music section -->
+
+<section class="section">
+  <div class="container">
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+      <div class="container">
+        <div class="form-group text-center">
+          <div class="login-logo">
+            <a href="#">
+              <img class="align-content" src="../img/logo-rs.png" width="200" height="200" alt="">
+            </a>
+          </div>
+          <div class="card-header">
+            <div class="card-title">
+              <h3 class="text-center">Rapid Antigen Screening Result Verification</h3>
+            </div>
+            <hr>
+            <form action="#" method="post" novalidate="novalidate">
+              <div class="form-group">
+                <label>Nama Pasien (Full Name)</label>
+                <input readonly type="text" class="form-control" value="Aziz Jamil">
+              </div>
+              <div class="form-group">
+                <label>NIK/ID (Identity)</label>
+                <input readonly type="text" class="form-control" value="3175062211930002">
+              </div>
+              <div class="form-group">
+                <label>Tanggal Lahir (Birth Date)</label>
+                <input readonly type="text" class="form-control" value="22 November 1993">
+              </div>
+              <div class="form-group">
+                <label>Handphone (Mobile)</label>
+                <input readonly type="text" class="form-control" value="082260570056">
+              </div>
+              <div class="form-group">
+                <label>Lokasi (Location)</label>
+                <input readonly type="text" class="form-control" value="RSU Bunda Jakarta">
+              </div>
+              <div class="form-group">
+                <label>Tanggal Pengambilan Sample (Sampling Date)</label>
+                <input readonly type="text" class="form-control" value="28/07/2021 / 11:55:32">
+              </div>
+              <div class="form-group">
+                <label>Tanggal Hasil (Result Date)</label>
+                <input readonly type="text" class="form-control" value="28/07/2021 / 12:05:11">
+              </div>
+              <div class="form-group">
+                <label>Rapid Antigen</label>
+                <input readonly type="text" class="form-control" value="Negatif (Negative)">
+              </div>
+              <div class="form-group">
+                <label>Kesimpulan (Result)</label>
+                <input readonly type="text" class="form-control btn btn-flat" value="Negatif (Negative)">
+              </div>
+              <div class="form-group text-align-center" style="width: 100%;">
+                <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Download PDF</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- footer -->
+<footer>
+  <div class="footer-bottom text-center">
+    Copyright &copy; 2019 RSU Bunda Jakarta
+  </div>
+</footer>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
+
+</html>
